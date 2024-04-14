845ac932ca46f9e7721e3c227bf64114e40d484b
Lyall <stefancandea2001@gmail.com>
2024-04-15 00:48:26 +0300

    Basic tile hover system

--- grid/GridColliderGenerator.gd
+++ grid/GridColliderGenerator.gd
@@ -6,24 +6,41 @@ extends Node3D
 @export var height : int
 @export var width : int
 @export var grid_size : float = 1.5 # this is the size of a tile
+@export var mat_selected : Material
 
-# how tf do i make a table
+var tiles_list = []
 
 
 func _ready():
 
-	for i in range(height):
-		for j in range(width):
+	for i in range(width):
+		tiles_list.append([])
+		for j in range(height):
 			var curTile : Node3D = tile.instantiate()
 			add_child(curTile)
-			curTile.position = Vector3( (j - float(width-1) / 2) * grid_size , 0 , (i - float(height-1) / 2 ) * grid_size )
-			(curTile.get_node(^"Area3D") as Area3D).mouse_entered.connect(TileHovered)
+			curTile.position = Vector3( (i - float(width-1) / 2) * grid_size , 0 , -1 * (j - float(height-1) / 2 ) * grid_size )
 
-func _physics_process(delta):
-
-	pass
+			tiles_list[i].append(curTile)
 
 
+			(curTile.get_node(^"Area3D") as Area3D).mouse_entered.connect(func(): TileHovered(i,j))
+			(curTile.get_node(^"Area3D") as Area3D).mouse_exited.connect(func(): TileUnhovered(i,j))
+			#store in table
 
 func TileHovered(x , y):
+	((tiles_list[x][y] as Node3D).get_node(^"MeshInstance3D") as MeshInstance3D).set_surface_override_material(0,mat_selected)
 	print("tile hovered xy: " ,x ,y)
+
+func TileUnhovered(x , y):
+	((tiles_list[x][y] as Node3D).get_node(^"MeshInstance3D") as MeshInstance3D).set_surface_override_material(0,null)
+	#print("tile unhovered xy: " ,x ,y)
+	pass
+
+func PlayOnTile(): #put data from card, on x y; 3 variables
+	pass
+
+func CheckSpawn():
+	pass
+
+func MovePawns():
+	pass
